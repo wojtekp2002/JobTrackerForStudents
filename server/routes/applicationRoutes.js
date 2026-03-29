01ab63d79452ed24acaaaef8f276fc739322206c
wojtek pasieka <wojtek.pasiu@gmail.com>
2026-03-29 21:35:42 +0200

    feat: getMyApplications

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { applyToJob } from "../controllers/applicationController.js";
+import { applyToJob, getMyApplications } from "../controllers/applicationController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/apply/:jobId", protect, applyToJob);
+router.get("/my-applications", protect, getMyApplications);
 
 export default router;
